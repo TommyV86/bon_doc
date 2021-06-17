5d249715b4967cdbfd6991e4c72017b4fc217e89
Tommy Vichidvongsa <tommychel@hotmail.fr>
2021-06-17 14:55:46 +0200

    maj PatientFixtures pour la boucle(ok)

--- src/DataFixtures/PatientFixtures.php
+++ src/DataFixtures/PatientFixtures.php
@@ -12,18 +12,21 @@ class PatientFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $patient = new Patient(); 
-        $patient->setNom('tutu');
-        $patient->setPrenom('toto');
-        $patient->setAdresse("15 rue de la porte Gellée, 44100 NANTES");
-        $patient->setTelephone('0102030405');
-        $patient->setEmail('tutu@tutu.fr');
-        $patient->setHash('azerty123.');
-        $patient->setDateInscription(new \DateTime());
-        $patient->setDateNaissance(new \Datetime('1990-03-27'));
-        $patient->setSexe('Male');
+        for($i = 1; $i < 10; $i++){
 
-        $manager->persist($patient);
-        $manager->flush();
+            $patient = new Patient(); 
+            $patient->setNom('tutu');
+            $patient->setPrenom('toto');
+            $patient->setAdresse("15 rue de la porte Gellée, 44100 NANTES");
+            $patient->setTelephone('0102030405');
+            $patient->setEmail('tutu@tutu.fr');
+            $patient->setHash('azerty123.');
+            $patient->setDateInscription(new \DateTime());
+            $patient->setDateNaissance(new \Datetime('1990-03-27'));
+            $patient->setSexe('Male');
+
+            $manager->persist($patient);
+            $manager->flush();
+        }
     }
 }
