0c29e77ec9f2226d96ddf2f558db44227d391c01
Decock Vincent <vincentdecock@outlook.fr>
2021-06-17 16:34:36 +0200

    ajout fix

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,32 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\RDV;
+use App\Entity\Medecin;
+use App\Entity\Patient;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 5; $i++) {
+            $rdv = new Rdv();
+            $rdv->setDateRdv(new \DateTime('2021-10-12'));
+            $rdv->setHeureRdv(new \DateTime('15:50'));
 
-        $manager->flush();
+            $medecin = new Medecin();
+            $medecin->setNom("doc");
+            $nomMedecin = $medecin->getNom();
+
+            $rdv->setMedecin($medecin);
+
+            $patient = new Patient();
+            $patient->setNom("pat");
+
+            $rdv->setPatient($patient);
+
+            $manager->persist($rdv);
+            $manager->flush();
+        }
     }
 }
