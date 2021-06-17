3ccd63690c93c13dc6fdbd355870c595a17ec640
Tommy Vichidvongsa <tommychel@hotmail.fr>
2021-06-17 13:45:41 +0200

    maj PatientFixtures (nonfini)

--- src/DataFixtures/PatientFixtures.php
+++ src/DataFixtures/PatientFixtures.php
@@ -8,14 +8,18 @@ use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class PatientFixtures extends Fixture
-{
+{   
 
     public function load(ObjectManager $manager)
     {
-        $patient = new Patient();
+        $patient = new Patient(); 
+        $patient->setNom('tutu');
+        $patient->setPrenom('toto');
+        $patient->setTelephone('0102030405');
         $patient->setEmail('tutu@tutu.fr');
-
-
+        $patient->setHash('azerty123.');
+        $patient->setDateInscription(new \DateTime());
+        $patient->setDateNaissance(new \Datetime('1990-03-27'));
         $manager->persist($patient);
         $manager->flush();
     }
