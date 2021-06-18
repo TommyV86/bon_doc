0dc5ea93ee2d5f93f8a6ae1a50d590a610a2f44f
Decock Vincent <vincentdecock@outlook.fr>
2021-06-18 08:58:37 +0200

    ajout de rdvfixtures

---
+++ src/DataFixtures/RdvFixtures.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\RDV;
+use App\Entity\Medecin;
+use App\Entity\Patient;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 5; $i++) {
+           
+
+            $medecin = new Medecin();
+            $medecin->setNom('VERSETTI');
+            $medecin->setPrenom('Tommy');
+            $medecin->setSpecialite('Dentiste');
+            $medecin->setSexe("masculin");
+            $medecin->setAdresse('15 rue de la porte gellée, 44100 NANTES');
+            $medecin->setTelephone('0320568179');
+            $medecin->setEmail('tutu@tutu.fr');
+            $medecin->setDateInscription(new \DateTime());
+            $medecin->setDateNaissance(new \Datetime('1990-03-27'));
+            $manager->persist($medecin);
+
+
+            $patient = new Patient(); 
+            $patient->setNom('tutu');
+            $patient->setPrenom('toto');
+            $patient->setAdresse("15 rue de la porte Gellée, 44100 NANTES");
+            $patient->setTelephone('0102030405');
+            $patient->setEmail('tutu@tutu.fr');
+            $patient->setHash('azerty123.');
+            $patient->setDateInscription(new \DateTime());
+            $patient->setDateNaissance(new \Datetime('1990-03-27'));
+            $patient->setSexe('Male');
+            $manager->persist($patient);
+
+            $rdv = new Rdv();
+            $rdv->setDateRdv(new \DateTime('2021-10-12'));
+            $rdv->setHeureRdv(new \DateTime('15:50'));
+
+            $rdv->setMedecin($medecin);
+            $rdv->setPatient($patient);
+
+            $manager->persist($rdv);
+        }
+        $manager->flush();
+    }
+}
