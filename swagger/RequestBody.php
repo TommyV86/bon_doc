e4f61433f12060ec7496e5c92fe8d60739e38950
Decock Vincent <vincentdecock@outlook.fr>
2021-06-22 16:19:23 +0200

    documentation API

---
+++ swagger/RequestBody.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license Apache 2.0
+ */
+
+/**
+ *
+ * @OA\RequestBody(
+ *     request="RdvDTO",
+ *     description="RdvDTO object that needs to be added to the store",
+ *     required=true,
+ *     @OA\JsonContent(ref="#/components/schemas/RdvDTO")
+ *     )
+ * )
+ */
